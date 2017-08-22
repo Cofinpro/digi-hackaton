cf05198c6b84f55020425a2fefb756936abf11c5
Markus Koschier <markus.koschier@cofinpro.de>
2017-08-22 14:12:08 +0200

    added html snippet

--- questions/src/app/app.module.ts
+++ questions/src/app/app.module.ts
@@ -5,11 +5,13 @@ import {RiskComponent} from "./risk/risk.component";
 import {Routes, RouterModule, RouterOutlet} from "@angular/router";
 import {PagenotfoundComponent} from "./pagenotfound/pagenotfound.component";
 import {AmountComponent} from "./amount/amount.component";
+import { RecommendationComponent } from './recommendation/recommendation.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'amount', pathMatch: 'full'},
   { path: 'amount', component: AmountComponent },
   { path: 'risk', component: RiskComponent },
+  { path: 'recommendation', component: RecommendationComponent },
   { path: '**', component: PagenotfoundComponent }
 ];
 
@@ -23,6 +25,6 @@ const appRoutes: Routes = [
   ],
   providers: [],
   bootstrap: [AppComponent],
-  declarations: [AppComponent, AmountComponent, RiskComponent, PagenotfoundComponent]
+  declarations: [AppComponent, AmountComponent, RiskComponent, PagenotfoundComponent, RecommendationComponent]
 })
 export class AppModule { }
