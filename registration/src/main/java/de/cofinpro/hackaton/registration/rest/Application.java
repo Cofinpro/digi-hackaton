ae5ca6c30326b163ad02d1bfc93fc06de9e4938e
Gregor Tudan <gregor.tudan@cofinpro.de>
2017-08-22 10:55:42 +0200

    add link to ozark fraction

---
+++ registration/src/main/java/de/cofinpro/hackaton/registration/rest/Application.java
@@ -0,0 +1,10 @@
+package de.cofinpro.hackaton.registration.rest;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * @author Gregor Tudan, Cofinpro AG
+ */
+@ApplicationPath("/")
+public class Application extends javax.ws.rs.core.Application {
+}
