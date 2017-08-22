06e0f6866cafda82674a50ed3f77d7ecde4664f3
dolah <david.olah@cofinpro.de>
2017-08-22 11:18:35 +0200

    Amount Component

--- questions/src/app/app.module.ts
+++ questions/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AmountComponent } from './amount/amount.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AmountComponent
   ],
   imports: [
     BrowserModule
