d74824347e70bb940dd86e77b7bfd535a1fbcc3a
Gregor Tudan <gregor.tudan@cofinpro.de>
2017-08-22 22:44:17 +0200

    add registration json

--- registration/src/main/java/de/cofinpro/hackaton/registration/db/DepotEntity.java
+++ registration/src/main/java/de/cofinpro/hackaton/registration/db/DepotEntity.java
@@ -1,5 +1,7 @@
 package de.cofinpro.hackaton.registration.db;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.io.Serializable;
